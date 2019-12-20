3cee458da74f1b783a6caaa3c157100c880f9bea
kyle <kyle@solidstategroup.com>
2019-12-20 12:01:53 +0000

    Blog wip

--- project/polyfill.js
+++ project/polyfill.js
@@ -16,7 +16,7 @@ if (typeof window !== 'undefined') {
 
 if (Project.freshChat && typeof window !== 'undefined') {
     function initFreshChat() {
-        window.fcWidget.init(Project.freshChat);
+        window.fcWidget && window.fcWidget.init(Project.freshChat);
     }
     function initialize(i, t) { let e; i.getElementById(t) ? initFreshChat() : ((e = i.createElement('script')).id = t, e.async = !0, e.src = 'https://wchat.freshchat.com/js/widget.js', e.onload = initFreshChat, i.head.appendChild(e)); } function initiateCall() { initialize(document, 'freshchat-js-sdk'); }window.addEventListener ? window.addEventListener('load', initiateCall, !1) : window.attachEvent('load', initiateCall, !1);
 }
