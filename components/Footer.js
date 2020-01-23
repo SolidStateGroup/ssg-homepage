e0e329d69d3a81adf2c45b5b226cd706b3dffd3c
andrewmoffatt <andrew@solidstategroup.com>
2020-01-23 14:56:51 +0000

    footer refactor II

--- components/Footer.js
+++ components/Footer.js
@@ -16,26 +16,28 @@ const Footer = class extends React.Component {
         return (
           <div className="footer-container">
               <footer className={`${className} clearfix footer`}>
-                  <div className="flex-row justify-content-center">
-                      <div className="flex-column">
-                          <img src="/static/images/svg-icons/partner-icon.svg" alt="something"/>
-                      </div>
-                      <div className="flex-column px-5">
-                          <h3 className="footer__title">Interested in working with us?</h3>
-                          <p className="footer__text">Are you a large enterprise looking for a technology partner or a
-                              consultancy looking for experienced, highly skilled developers to supplement your
-                              team.</p>
-                      </div>
-                      <div className="flex-column">
-                          <dl>
-                              <dt>Call us on</dt>
-                              <dd>+44 (0)207 613 7222</dd>
-                          </dl>
-                          <dl>
-                              <dt>Email us at</dt>
-                              <dd>projects@solidstategroup.com</dd>
-                          </dl>
-                          <ButtonPrimary>Get in touch</ButtonPrimary>
+                  <div className="container">
+                      <div className="row">
+                          <div className="col text-center">
+                              <img src="/static/images/svg-icons/partner-icon.svg" alt="something"/>
+                          </div>
+                          <div className="col col-lg-6 px-5">
+                              <h3 className="footer__title">Interested in working with us?</h3>
+                              <p className="footer__text">Are you a large enterprise looking for a technology partner or a
+                                  consultancy looking for experienced, highly skilled developers to supplement your
+                                  team.</p>
+                          </div>
+                          <div className="col">
+                              <dl>
+                                  <dt>Call us on</dt>
+                                  <dd>+44 (0)207 613 7222</dd>
+                              </dl>
+                              <dl>
+                                  <dt>Email us at</dt>
+                                  <dd>projects@solidstategroup.com</dd>
+                              </dl>
+                              <ButtonPrimary>Get in touch</ButtonPrimary>
+                          </div>
                       </div>
                   </div>
               </footer>
--- components/FooterLegal.js
+++ components/FooterLegal.js
@@ -6,7 +6,7 @@ const FooterLegal = class extends React.Component {
 
     static propTypes = {
         className: propTypes.string,
-    }
+    };
 
     componentDidMount() {
     }
@@ -15,18 +15,24 @@ const FooterLegal = class extends React.Component {
         const { className } = this.props;
         return (
           <div className={`${className} footer-legal`}>
-            <div className="flex-row">
-                <div className="flex-column mr-auto">
-                    <p className="footer__text mr-auto">&copy; Solid State Group 2020.  Company number 4510864.  <a href="/" className="footer__text">Privacy Policy</a></p>
-                </div>
-                <div className="flex-column">
-                    <ul className="list-inline">
-                        <li className="list-inline-item"><img src="/static/images/platforms/behance.svg" alt="Behance"/></li>
-                        <li className="list-inline-item"><img src="/static/images/platforms/dribbble.svg" alt="Dribbble"/></li>
-                        <li className="list-inline-item"><img src="/static/images/platforms/github.svg" alt="GitHub"/></li>
-                    </ul>
-                </div>
-            </div>
+              <div className="container">
+                  <div className="flex-row">
+                      <div className="flex-column mr-auto">
+                          <p className="footer__text mr-auto">&copy; Solid State Group 2020. Company number 4510864. <a
+                            href="/" className="footer__text">Privacy Policy</a></p>
+                      </div>
+                      <div className="flex-column">
+                          <ul className="list-inline">
+                              <li className="list-inline-item"><img src="/static/images/platforms/behance.svg"
+                                                                    alt="Behance"/></li>
+                              <li className="list-inline-item"><img src="/static/images/platforms/dribbble.svg"
+                                                                    alt="Dribbble"/></li>
+                              <li className="list-inline-item"><img src="/static/images/platforms/github.svg"
+                                                                    alt="GitHub"/></li>
+                          </ul>
+                      </div>
+                  </div>
+              </div>
           </div>
         );
     }
