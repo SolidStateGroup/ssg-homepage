cb395ae35ae07c77699003db3647b824b3fbcf38
kyle <kyle@solidstategroup.com>
2020-02-11 18:07:28 +0000

    Initialise mapbox on scroll

---
+++ components/withScrollHelper.js
@@ -0,0 +1,34 @@
+export default (WrappedComponent) => {
+    class HOC extends React.Component {
+        static displayName = 'withFoo';
+
+        constructor(props) {
+            super(props);
+            this.state = {};
+        }
+
+        componentDidMount() {
+            this.listener = window.addEventListener('scroll', this.onScroll);
+        }
+
+        onScroll = () => {
+            if (!this.scrolled) {
+                this.scrolled = true;
+                this.setState({ reached: true });
+                window.addEventListener('scroll', this.onScroll);
+            }
+        }
+
+        render() {
+            return this.state.reached ? (
+                <WrappedComponent
+                  ref="wrappedComponent"
+                  {...this.props}
+                  {...this.state}
+                />
+            ) : <div ref={target => this.target = target} />;
+        }
+    }
+
+    return HOC;
+};
