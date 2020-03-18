75cf44e65eea6f273ccbceae7f89bf4467e35410
andrew <andrew@solidstategroup.com>
2020-03-18 18:46:20 +0000

    Meow now project content

--- pages/projects/meownow.js
+++ pages/projects/meownow.js
@@ -41,7 +41,7 @@ const TheComponent = class extends Component {
                                 <TextListItem listItemText={'Docker,'}/>
                             </React.Fragment>
                         }
-                        className="hero--publicate"
+                        className="hero--meownow"
                         projectImage={"/static/images/projects/meownow/meownow-1@1x.png"}
                     />
 
@@ -54,35 +54,41 @@ const TheComponent = class extends Component {
                             'The creative brief was to design a new brand identity, product packaging and product website for a cat food subscription business. The brand must feel real, high quality and generate enough confidence for website visitors to commit to buying the products and sign up for a weekly, monthly or annual subscription.'}
                         />
                         <ProjectFullImageSection
+                            title={'Design Language'}
+                            subTitle={'01 / Style guide'}
+                            imageUrl={'/static/images/projects/meownow/meownow_brand_board.png'}
+                            imageAltText={'Responsive UI'}
+                        />
+                        <ProjectFullImageSection
                             title={'Mobile UI'}
-                            subTitle={'01 / Ad Creative'}
+                            subTitle={'02 / Responsive Website'}
                             imageUrl={'/static/images/projects/meownow/tails_mobile.png'}
                             imageAltText={'Responsive UI'}
                         />
                         <ProjectTextSection
-                            subTitle="02 / Solution"
+                            subTitle="03 / Solution"
                             title="The Experiments"
                             description={'We undertook a 5 day design sprint, during which time we developed a number of brand names before settling on MeowNow.  We then set about creating a number of brand routes beginning with 6 polarising ideas and gradually knocking those down until the client was settled on one route, which was then iterated until sign off.  Once we had a brand we began to create a design language, once again developing multiple routes for the client and eventually progressing with one. This would eventually be used to design the ad creative and the responsive website.  As part of the creative requirements we needed to show some legitimate product packaging that you might expect to see in a real life subscription product. We set about designing cans, bags and boxes that would provide the backdrop for the marketing efforts.'}
                         />
                     </div>
 
-                    <div className="panel panel--gradient-publicate pb-0 text-center mb-5">
+                    <div className="panel panel--gradient-meownow pb-0 text-center mb-5">
                         <div className="container">
                             <img src="/static/images/projects/meownow/tails_mobile_multiple.png" alt={'Publicate UI'} className="img-fluid"/>
                         </div>
                     </div>
 
                     <ProjectFullImageSection
-                        subTitle={'03 / Ad Creative'}
+                        subTitle={'04 / Ad Creative'}
                         title={'Display Ads'}
-                        imageUrl={'/static/images/projects/meownow/tails_cat-ads.jpg'}
+                        imageUrl={'/static/images/projects/meownow/tails_cat-ads@1x.png'}
                         imageAltText={'Responsive UI'}
                     />
 
                     <div className="p-5 flex-row">
                         <div className="container text-center">
-                            <img src="/static/images/projects/meownow/meownow-facebook-1.png" alt="Meownow Ad" className="pr-2"/>
-                            <img src="/static/images/projects/meownow/meownow-facebook-2.png" alt="Meownow Ad 2" className="pl-2"/>
+                            <img src="/static/images/projects/meownow/meownow-facebook-1@1x.png" alt="Meownow Ad" className="pr-2"/>
+                            <img src="/static/images/projects/meownow/meownow-facebook-2@1x.png" alt="Meownow Ad 2" className="pl-2"/>
                         </div>
                     </div>
 
