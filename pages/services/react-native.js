72c2b863bf1cadf8fd2bef8d4df02151f856e787
andrewmoffatt <andrew@solidstategroup.com>
2020-06-02 11:38:26 +0100

    Content for React Native page

--- pages/services/react-native.js
+++ pages/services/react-native.js
@@ -36,6 +36,18 @@ const projects = [
         projectType: ['Web App Development, ', 'API Development, ', 'Mobile App Development']
 
     },
+    {
+        ButtonComponent: ButtonPrimary,
+        href: '/projects/change-please',
+        projectImage: '/static/images/projects/change-please/Rotato-Snapshot@1x.png',
+        srcSet: '/static/images/projects/change-please/Rotato-Snapshot@1x.png 1x, /static/images/projects/change-please/Rotato-Snapshot@2x.png 2x,',
+        clientImage: '/static/images/clients/change-please.svg',
+        subTitle: 'The lean, mean, face detecting, debt tracking machine',
+        title: 'Changing the [face] of debt tracking',
+        blobColour: "#908DF7",
+        tags: ['DESIGN', 'MOBILE', 'AUGMENTED REALITY'],
+        projectType: ['UX Design, ', 'UI design, ', 'Branding, ', 'Mobile App Development']
+    },
 ];
 
 const MobileAppsPage = class extends React.Component {
@@ -59,7 +71,7 @@ const MobileAppsPage = class extends React.Component {
                               <div className="col-lg-6 offset-lg-3">
                                   <p className="text-light">We use React Native to build amazing mobile apps. Why?
                                       React Native is a real mobile app. Unlike web app wrappers, React Native uses
-                                      native platform libraries, creating a completely native experience for your
+                                      native platform libraries to create a completely native experience for your
                                       users.</p>
                               </div>
                           </div>
@@ -68,6 +80,13 @@ const MobileAppsPage = class extends React.Component {
               </div>
               <div className="container">
                   <ServiceItem
+                    serviceImage={'/static/images/services/rn-apps.png'}
+                    className={'flex-row'}
+                    imageClassName={'img__shadow '}
+                    subTitle={'Tried and trusted'}
+                    serviceDescription={'React Native is a framework developed by Facebook which enables building natively rendered apps using JavaScript.  Since it first appeared in 2015 it has grown in popularity and is now the framework of choice for alot of the mobile apps on your home screen and some of the best technology companies in the world.'}/>
+
+                  <ServiceItem
                     serviceImage={'/static/images/services/react-native.png'}
                     className={'flex-row-reverse'}
                     imageClassName={'img__shadow '}
@@ -75,7 +94,6 @@ const MobileAppsPage = class extends React.Component {
                     serviceDescription={'React allows us to build mobile apps for iOS and Android at the same time with a shared application logic. There\'s an added benefit of shared application code for web applications as well.'}/>
 
                   <ServiceItem
-                    className="section section--grey"
                     serviceImage={'/static/images/projects/tone-and-sculpt/t&s-phones.jpg'}
                     subTitle={'Ship fast, for less'}
                     serviceDescription={'React Native allows us to build incredibly efficiently, so you can get to market in record time and the shared codebase could reduce the cost of your product build by 40%!\n' +
@@ -83,7 +101,7 @@ const MobileAppsPage = class extends React.Component {
               </div>
 
               <div className="container">
-                  <h3 className="section__title--dark text-center mb-5 mt-5">Built with React Native</h3>
+                  <h3 className="section__title--dark text-center mb-5 mt-5">Recent React Native projects</h3>
                   {projects.map((p) => (
                     <ProjectItem ButtonComponent={p.ButtonComponent} projectImage={p.projectImage}
                                  subTitle={p.subTitle}
