28b4a83725b43e97a42801c2955c776cda1a1fc9
andrewmoffatt <andrew@solidstategroup.com>
2020-06-24 17:13:54 +0100

    Add mapbox API key

--- env/project_prod.js
+++ env/project_prod.js
@@ -1,5 +1,6 @@
 module.exports = global.Project = {
     siteName: 'Solid State Group',
+    mapbox: 'pk.eyJ1IjoiYW1vZmYxMTEiLCJhIjoiY2s1ZjVsZzdqMDdiejNybnd6NmFycGozNyJ9.my-4G97iJwCW4TosD-iIwQ',
     cookieDomain: '.solidstategroup.com',
     canonicalUrl: 'https://www.solidstategroup.com',
     ga: 'UA-152927-1',
