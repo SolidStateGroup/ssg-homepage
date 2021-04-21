5b5822b542d23b50dd38cb1dfa4c1c3692f39649
andrewmoffatt <andrew@solidstategroup.com>
2021-04-21 13:15:55 +0100

    remove comments

--- pages/_document.js
+++ pages/_document.js
@@ -11,12 +11,13 @@ class MyDocument extends Document {
             <Html lang="en">
                 <Head />
                 <body>
-                <!-- Google Tag Manager (noscript) -->
-                <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NXCB72"
-                                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-                <!-- End Google Tag Manager (noscript) -->
+                    <noscript><iframe
+                      src="https://www.googletagmanager.com/ns.html?id=GTM-NXCB72"
+                      height="0" width="0" style="display:none;visibility:hidden"
+                    />
+                    </noscript>
 
-                <Main />
+                    <Main />
                     <NextScript />
                 </body>
             </Html>
