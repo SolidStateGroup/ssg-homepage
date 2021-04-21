6c18b9a710b61317479a741596d7255bec3efb47
andrewmoffatt <andrew@solidstategroup.com>
2021-04-21 11:59:05 +0100

    Wrap code snippets

--- pages/_app.js
+++ pages/_app.js
@@ -27,18 +27,24 @@ class MyApp extends App {
                         <meta httpEquiv="x-ua-compatible" content="ie=edge"/>
 
                         <script>
-                            window.dataLayer = window.dataLayer || [];
-                            window.dataLayer.push({
-                            'event' : 'dataLayer'
-                        });
+                            if (typeof window !=='undefined') {
+                                    window.dataLayer = window.dataLayer || [];
+                                    window.dataLayer.push({
+                                    'event' : 'dataLayer'
+                                });
+                            }
                         </script>
 
                         <!-- Google Tag Manager -->
-                        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                        })(window,document,'script','dataLayer','GTM-NXCB72');</script>
+                        <script>
+                            if (typeof window !=='undefined') {
+                                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                                      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                                  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                                })(window,document,'script','dataLayer','GTM-NXCB72');
+                            }
+                            </script>
                         <!-- End Google Tag Manager -->
 
                         <meta
