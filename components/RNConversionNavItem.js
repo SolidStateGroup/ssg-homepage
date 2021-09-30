d6be94fde41610c4851efd0f73c91127f70544c5
kyle-ssg <kyle@solidstategroup.com>
2021-09-30 14:40:09 +0100

    Prefetch other links

--- components/RNConversionNavItem.js
+++ components/RNConversionNavItem.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import cn from 'classnames';
 import propTypes from 'prop-types';
+import Link from 'next/link';
 import StarRatingListItem from './StartRatingListItem';
 import ButtonHexagonSidebar from './ButtonHexagonSidebar';
-import Link from 'next/link';
 
 const _propTypes = {
-  className: propTypes.string,
-  isActive: propTypes.bool,
-  number: propTypes.string,
-  title: propTypes.string,
-  icon: propTypes.string,
-  href: propTypes.string,
+    className: propTypes.string,
+    isActive: propTypes.bool,
+    number: propTypes.string,
+    title: propTypes.string,
+    icon: propTypes.string,
+    href: propTypes.string,
 };
 
 const RNConversionNavItem = global.RNConversionNavItem = class extends React.PureComponent {
@@ -20,27 +20,27 @@ const RNConversionNavItem = global.RNConversionNavItem = class extends React.Pur
   static propTypes = _propTypes;
 
   render() {
-    const { children, ButtonComponent, isActive, number, title, icon, href, ...rest } = this.props;
-    return (
+      const { children, ButtonComponent, isActive, number, title, icon, href, ...rest } = this.props;
+      return (
 
-      <Link prefetch={false} href={this.props.href}>
-        <li className="nav-item">
-          <a href="#" className={`nav-link ${this.props.isActive ? ' active' : ''}`}>
-            <div className="flex-row">
-              <ButtonHexagonSidebar
-                ButtonHexagonActive={isActive}
-                icon={`${this.props.icon} ${this.props.isActive ? ' text-primary' : ''}`}
-              />
-              <div className="col">
-                <h6 className="text-light mb-0">{this.props.number}</h6>
-                <h6 className="text-navy-secondary mb-0">{this.props.title}</h6>
-              </div>
-            </div>
-          </a>
-        </li>
-      </Link>
+          <Link href={this.props.href}>
+              <li className="nav-item">
+                  <a href="#" className={`nav-link ${this.props.isActive ? ' active' : ''}`}>
+                      <div className="flex-row">
+                          <ButtonHexagonSidebar
+                            ButtonHexagonActive={isActive}
+                            icon={`${this.props.icon} ${this.props.isActive ? ' text-primary' : ''}`}
+                          />
+                          <div className="col">
+                              <h6 className="text-light mb-0">{this.props.number}</h6>
+                              <h6 className="text-navy-secondary mb-0">{this.props.title}</h6>
+                          </div>
+                      </div>
+                  </a>
+              </li>
+          </Link>
 
-    );
+      );
   }
 };
 export default RNConversionNavItem;
