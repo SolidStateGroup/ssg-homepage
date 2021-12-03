cb7b8abfe8d7a6ebd91907648fd45a6637ac0fa3
niallquinn <niall@solidstategroup.com>
2021-12-03 16:33:45 +0000

    Electron Connect case

--- pages/projects/electron-connect.js
+++ pages/projects/electron-connect.js
@@ -1,13 +1,13 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import propTypes from 'prop-types';
 import Page from '../../components/Page';
 import ProjectHero from '../../components/ProjectHero';
-import {TextListItem, WideListItem} from '../../components/ListItem';
+import { TextListItem, WideListItem } from '../../components/ListItem';
 import ClientQuote from '../../components/ClientQuote';
 import ProjectDetailSectionItem, {
     ProjectColourBlockSection,
     ProjectTechSection,
-    ProjectTextSection
+    ProjectTextSection,
 } from '../../components/ProjectDetailSectionItem';
 import Footer from '../../components/Footer';
 import ProjectNav from '../../components/ProjectNav';
@@ -29,81 +29,127 @@ const TheComponent = class extends Component {
             <React.Fragment>
                 <Page title="Electron Connect" canonical="projects/electron-connect">
                     <ProjectHero
-                        clientImage="/static/images/clients/white/electron-white.png"
-                        projectTitle={'Tapping into the future of the electricity market'}
-                        projectType={'Responsive Web Application'}
-                        ProjectServiceListItem={
-                            <React.Fragment>
-                                <TextListItem listItemText={'UX design,'}/>
-                                <TextListItem listItemText={'UI design'}/>
-                            </React.Fragment>
-                        }
-                        className="hero--electron-connect"
-                        projectImage={'/static/images/projects/electron-connect/1.png'}
-                        srcSet={'/static/images/projects/electron-connect/1.png 1x, /static/images/projects/electron-connect/1@2x.png 2x'}
+                      clientImage="/static/images/clients/white/electron-white.png"
+                      projectTitle="Tapping into the future of the electricity market"
+                      projectType="Responsive Web Application"
+                      ProjectServiceListItem={(
+                          <React.Fragment>
+                              <TextListItem listItemText="UX design,"/>
+                              <TextListItem listItemText="UI design"/>
+                          </React.Fragment>
+)}
+                      className="hero--electron-connect"
+                      projectImage="/static/images/projects/electron-connect/1.png"
+                      srcSet="/static/images/projects/electron-connect/1.png 1x, /static/images/projects/electron-connect/1@2x.png 2x"
                     />
 
                     <div className="container">
                         <div id="sectionOneTrigger">
                             <ProjectTextSection
-                                className="sectionOneAnimation"
-                                subTitle="00 / Overview"
-                                description={'There are around a dozen wind generators in the Orkneys, they generate more electricity as the wind speed increases. There are about 120 small “assets” – such as storage heaters in people’s homes, when switched on they consume energy. When the generators are generating too much and there is too little local consumption, the network tells some of the wind generators to turn off (a “constraint”), which costs them money. \n' +
-                                '\n' +
-                                'What the underlying platform does is to allow the generators to offer to pay the assets to turn on, which would in turn lead to the network removing the constraint (so the generators get paid). The state of the system is determined once a minute (an “epoch”), based on messages it receives from both the assets and generators.\n'}
+                              className="sectionOneAnimation"
+                              subTitle="00 / Overview"
+                              description={'Global energy systems are being transformed by the need to reach Net Zero.\n'
+                                + '\n'
+                                + 'Today’s semi-analogue energy management tools are increasingly unable to realise cost and carbon efficiencies in complex systems comprising millions of distributed energy assets.\n'
+                                + '\n'
+                                + 'As certain regions experience rising network constraints and are forced to curtail, and therefore waste renewable generation, it is increasingly recognised that new solutions are needed to address these market inefficiencies. \n'
+                                + '\n'
+                                + 'The ElectronConnect platform aims to deliver immediate solutions for local network constraints, enabling the users of the platform themselves to design multiple, repeatable market solutions. \n'
+                                + '\n'
+                                + 'Ultimately, the technology will deliver global, system-wide market infrastructure offering opportunities for innovation and efficiency which are not available in the undigitised systems of today.\n'}
                             />
 
                             <ProjectTextSection
-                                className="sectionTwoAnimation"
-                                subTitle="01 / Challenge"
-                                description={'Electron needed us to significantly upgrade the UI of their PoC platform. They needed design ideas that would make the UI standout as a slick, professional, interface. '}
+                              className="sectionTwoAnimation"
+                              subTitle="01 / Challenge"
+                              description="Following on from our previous work with Electron for the Trader platform (a ​​proof of concept local energy marketplace), which received awards for delivering an outstanding project in renewable energy, we were asked to help design the ElectronConnect product aimed at reorienting & digitising Global energy systems."
                             />
                         </div>
 
-                        <div id="sectionTwoTrigger">
-                            <ProjectDetailSectionItem
-                                className="sectionTwoAnimation"
-                                imageOneclassName="img__shadow"
-                                title={'UI Components'}
-                                imageOne={'/static/images/projects/electron-connect/Design system.png'}
-                                srcSetImageOne={'/static/images/projects/electron-connect/Design system.png 1x, /static/images/projects/electron-connect/Design system@2x.png 2x'}
-                                ProjectServiceListItem={
-                                    <React.Fragment>
-                                        <WideListItem
-                                            listItemText={'UX Design'}/>
-                                        <WideListItem
-                                            listItemText={'UI Style Guide'}/>
-                                        <WideListItem
-                                            listItemText={'UI Design'}/>
-                                        <WideListItem
-                                            listItemText={'Data Visualisation'}/>
-                                    </React.Fragment>}
-                            />
-                        </div>
+                        <ProjectDetailSectionItem
+                          description="We reviewed Electron’s initial requirements and wireframes for the product that they had put together and redesigned it from the ground up with UX best practices.
+
+We distilled the requirements into tangible UX artefacts such as personas and a low fidelity clickable prototype for all user types.
+"
+                          // imageOneclassName="img__shadow"
+                          title="UI Components"
+                          imageOne="/static/images/projects/electron-connect/3.png"
+                          srcSetImageOne="/static/images/projects/electron-connect/3.png 1x, /static/images/projects/electron-connect/3@2x.png 2x"
+                          // imageTwo="/static/images/projects/electron-connect/4.png"
+                          // srcSetImageTwo="/static/images/projects/electron-connect/4.png 1x, /static/images/projects/electron-connect/4@2x.png 2x"
+                          ProjectServiceListItem={(
+                              <React.Fragment>
+                                <WideListItem
+                                  listItemText="UX Design"
+                                />
+                                <WideListItem
+                                  listItemText="Personas"
+                                />
+                                <WideListItem
+                                  listItemText="User Journey Maps"
+                                />
+                                <WideListItem
+                                  listItemText="Low Fidelity Prototype"
+                                />
+                              </React.Fragment>
+                          )}
+                        />
+
+                        <ProjectDetailSectionItem
+                          description="We built on the previous design system we created for Electron’s Trader platform, adding to the existing components and design language to lay the foundation for the first iteration of the product. "
+                          className="flex-row-reverse"
+                          imageOneclassName="img__shadow"
+                          title="UX Research"
+                          imageOne="/static/images/projects/electron-connect/Design system.png"
+                          srcSetImageOne="/static/images/projects/electron-connect/Design system.png 1x, /static/images/projects/electron-connect/Design system@2x.png 2x"
+                          ProjectServiceListItem={(
+                              <React.Fragment>
+                                  <WideListItem
+                                    listItemText="UI Design"
+                                  />
+                                  <WideListItem
+                                    listItemText="Design System"
+                                  />
+                                  <WideListItem
+                                    listItemText="Data Visualisation"
+                                  />
+                                  <WideListItem
+                                    listItemText="High Fidelity Clickable Prototype"
+                                  />
+                              </React.Fragment>
+)}
+                        />
+
                     </div>
 
                     <ProjectFullImageSection
-                      title={'Dashboard UI'}
-                      subTitle={'UI Design'}
-                      imageUrl={'/static/images/projects/electron-connect/2.png'}
-                      srcSet={'/static/images/projects/electron-connect/2.png 1x, /static/images/projects/electron-connect/2@2x.png 2x'}
-                      imageAltText={'Dashboard UI'}
+                      title="Dashboard UI"
+                      subTitle="UI Design"
+                      imageUrl="/static/images/projects/electron-connect/2.png"
+                      srcSet="/static/images/projects/electron-connect/2.png 1x, /static/images/projects/electron-connect/2@2x.png 2x"
+                      imageAltText="Dashboard UI"
                     />
 
                     <div className="container sectionFourAnimation" id="sectionFourTrigger">
                         <ProjectTextSection
-                            subTitle="02 / Solution"
-                            description={'Using the Electron brand guidelines as a starting point, we designed and implemented a slick and professional dashboard interface. This included bespoke component design and data visualization.'}/>
-                        <ProjectDetailSectionItem
-                            subTitle={'03 / Design'}
-                            title={'Fonts & Colours'}
-                            className="flex-row-reverse"
-                            imageOne={'/static/images/projects/wazoku/wazoku-font.png'}
-                            description={'Summary of the fonts and colours used in the User Interface design of the application'}
+                          subTitle="02 / Solution"
+                          description="Electron are currently developing the dashboard in house, so once we delivered the design we set up a Zeplin project to easily share our assets and versioned changes with Electron’s developers.
+
+
+We produced a scalable design system and high fidelity prototype that Electron are currently using to build out the first version of ElectronConnect.
+"
                         />
-                        <ProjectColourBlockSection
-                            imageOne={'/static/images/projects/electronx/electronx_primary-colours.png'}
-                            imageTwo={'/static/images/projects/electronx/electronx_secondary-colours.png'}/>
+                        {/* <ProjectDetailSectionItem */}
+                        {/*  subTitle="03 / Design" */}
+                        {/*  title="Fonts & Colours" */}
+                        {/*  className="flex-row-reverse" */}
+                        {/*  imageOne="/static/images/projects/wazoku/wazoku-font.png" */}
+                        {/*  description="Summary of the fonts and colours used in the User Interface design of the application" */}
+                        {/* /> */}
+                        {/* <ProjectColourBlockSection */}
+                        {/*  imageOne="/static/images/projects/electronx/electronx_primary-colours.png" */}
+                        {/*  imageTwo="/static/images/projects/electronx/electronx_secondary-colours.png" */}
+                        {/* /> */}
                     </div>
 
                     <ProjectNav/>
